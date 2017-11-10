95be99d89558ac918b512d11a5f442dd7d334b9d
Anastasiya Belikava <Anastasiya_Belikava@epam.com>
2017-11-10 14:20:24 +0300

    first four strings on selenide

--- src/test/java/SomeTestClass.java
+++ src/test/java/SomeTestClass.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import org.testng.annotations.Test;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
@@ -7,6 +8,7 @@ public class SomeTestClass {
     @Test
     public void choseeMovieTest()
     {
+        Configuration.browser = "chrome";
         String movieName = "Звездные Войны";
         open("https://www.kinopoisk.ru/");
         $(".header-search-partial-component__search-field").setValue(movieName);
